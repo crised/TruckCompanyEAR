fc0fa89ddac1d0a618a2eca32be52294e54dfc29
Riccardo Merolla <riccardo.merolla@gmail.com>
2012-09-07 15:48:02 +0200

    add mock data for_user

--- truckcompany/src/main/resources/import.sql
+++ truckcompany/src/main/resources/import.sql
@@ -17,3 +17,5 @@
 
 -- You can use this file to load seed data into the database using SQL statements
 insert into Member_html5mobi (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212')
+
+insert into tc_user (id_user, username, password) values (1,'user1','user1')
