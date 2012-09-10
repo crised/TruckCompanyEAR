0749a1be6bae6d23d4d7426020c6f7c1ecb8adc9
ivan.ushakov <ivan.ushakov@gmail.com>
2012-09-10 21:11:42 +0400

    Test data script.

---
+++ Postgres Conf/test_data.sql
@@ -0,0 +1,10 @@
+--- user with credentials test:password
+INSERT INTO tc_user VALUES (1, 'test', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', false, NULL, 0);
+INSERT INTO tc_role VALUES (1, 'user');
+
+-- trucks
+INSERT INTO tc_truck VALUES (1, 'Mack', '111', false, NULL, 0);
+INSERT INTO tc_truck VALUES (2, 'Scania', '222', false, NULL, 0);
+INSERT INTO tc_truck VALUES (3, 'Volvo', '333', false, NULL, 0);
+
+
